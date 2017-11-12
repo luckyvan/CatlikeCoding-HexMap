d4f2c2701d2503d7e306ed3451c82b48c44fee37
luckyvan <fanchou1215@gmail.com>
2017-11-12 21:55:00 +0800

    3.1.4 Creating Slopes

--- Assets/Scripts/HexMesh.cs
+++ Assets/Scripts/HexMesh.cs
@@ -76,6 +76,7 @@ public class HexMesh : MonoBehaviour
         Vector3 bridge = HexMetrics.GetBridge(direction);
         Vector3 v3 = v1 + bridge;
         Vector3 v4 = v2 + bridge;
+        v3.y = v4.y = neighbor.Elevation * HexMetrics.elevationStep;
 
         AddQuad(v1, v2, v3, v4);
         AddQuadColor(cell.color, neighbor.color);
@@ -84,7 +85,9 @@ public class HexMesh : MonoBehaviour
         {
             if (direction <= HexDirection.E && nextNeighbor != null)
             {
-                AddTriangle(v2, v4, v2 + HexMetrics.GetBridge(direction.Next()));
+                Vector3 v5 = v2 + HexMetrics.GetBridge(direction.Next());
+                v5.y = nextNeighbor.Elevation * HexMetrics.elevationStep;
+                AddTriangle(v2, v4, v5);
                 AddTriangleColor(cell.color, neighbor.color, nextNeighbor.color);
             }
         }
