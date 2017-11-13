60bf7f396cc070e7069177dbd7cc7ad04f87a921
luckyvan <fanchou1215@gmail.com>
2017-11-13 09:14:29 +0800

    3.2 Terraced Edge Connections

--- Assets/Scripts/HexMetrics.cs
+++ Assets/Scripts/HexMetrics.cs
@@ -13,6 +13,10 @@ public static class HexMetrics
 
     public const float elevationStep = 5f;
 
+    public const int terracesPerSlope = 2;
+
+    public const int terraceSteps = terracesPerSlope * 2 + 1;
+
     public static Vector3[] corners = {
         new Vector3(0f, 0f, outerRadius),
         new Vector3(innerRadius, 0f, 0.5f * outerRadius),
@@ -47,5 +51,25 @@ public static class HexMetrics
     {
         return (corners[(int)direction] + corners[(int)direction + 1]) *
             blendFactor;
+    }
+
+    public const float horizontalTerraceStepSize = 1f / terraceSteps;
+
+    public const float verticalTerraceStepSize = 1f / (terracesPerSlope + 1);
+
+    public static Vector3 TerraceLerp(Vector3 a, Vector3 b, int step)
+    {
+        float h = step * HexMetrics.horizontalTerraceStepSize;
+        a.x += (b.x - a.x) * h;
+        a.z += (b.z - a.z) * h;
+        float v = ((step + 1) / 2) * HexMetrics.verticalTerraceStepSize;
+        a.y += (b.y - a.y) * v;
+        return a;
+    }
+
+    public static Color TerraceLerp(Color a, Color b, int step)
+    {
+        float h = step * HexMetrics.horizontalTerraceStepSize;
+        return Color.Lerp(a, b, h);
     }
 }
