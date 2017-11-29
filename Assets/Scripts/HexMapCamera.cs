7a46f1eebb01e38d172211c6c14f512cddbeeab6
luckyvan <fanchou1215@gmail.com>
2017-11-29 21:01:05 +0800

    5.2.2 Moving

--- Assets/Scripts/HexMapCamera.cs
+++ Assets/Scripts/HexMapCamera.cs
@@ -11,6 +11,12 @@ public class HexMapCamera : MonoBehaviour
 
     public float swivelMinZoom, swivelMaxZoom;
 
+    //public float moveSpeed;
+
+    public float moveSpeedMinZoom, moveSpeedMaxZoom;
+
+    public HexGrid grid;
+
     void Awake()
     {
         swivel = transform.GetChild(0);
@@ -24,6 +30,13 @@ public class HexMapCamera : MonoBehaviour
         {
             AdjustZoom(zoomDelta);
         }
+
+        float xDelta = Input.GetAxis("Horizontal");
+        float zDelta = Input.GetAxis("Vertical");
+        if (xDelta != 0f || zDelta != 0f)
+        {
+            AdjustPosition(xDelta, zDelta);
+        }
     }
 
     void AdjustZoom(float delta)
@@ -34,5 +47,33 @@ public class HexMapCamera : MonoBehaviour
         stick.localPosition = new Vector3(0f, 0f, distance);
         float angle = Mathf.Lerp(swivelMinZoom, swivelMaxZoom, zoom);
         swivel.localRotation = Quaternion.Euler(angle, 0f, 0f);
+    }
+
+    void AdjustPosition(float xDelta, float zDelta)
+    {
+        Vector3 direction = new Vector3(xDelta, 0f, zDelta).normalized;
+        float damping = Mathf.Max(Mathf.Abs(xDelta), Mathf.Abs(zDelta));
+        float distance =
+                Mathf.Lerp(moveSpeedMinZoom, moveSpeedMaxZoom, zoom) *
+                damping * Time.deltaTime;
+
+        Vector3 position = transform.localPosition;
+        position += direction * distance;
+        transform.localPosition = ClampPosition(position);
+    }
+
+    Vector3 ClampPosition(Vector3 position)
+    {
+        float xMax =
+                (grid.chunkCountX * HexMetrics.chunkSizeX - 0.5f) *
+                (2f * HexMetrics.innerRadius);
+        position.x = Mathf.Clamp(position.x, 0f, xMax);
+
+        float zMax =
+              (grid.chunkCountZ * HexMetrics.chunkSizeZ - 1) *
+              (1.5f * HexMetrics.outerRadius);
+        position.z = Mathf.Clamp(position.z, 0f, zMax);
+
+        return position;
     }
 }
