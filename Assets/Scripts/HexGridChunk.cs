9f103b077fe46f405525344dd92641d9b24d84f7
luckyvan <fanchou1215@gmail.com>
2017-12-14 15:18:41 +0800

    8.7.7 Blending River and Shore

--- Assets/Scripts/HexGridChunk.cs
+++ Assets/Scripts/HexGridChunk.cs
@@ -232,14 +232,19 @@ public class HexGridChunk : MonoBehaviour
 
         estuaries.AddTriangle(e1.v3, e2.v2, e2.v4);
         estuaries.AddTriangleUV(
-            new Vector2(0f, 0f), new Vector2(0f, 1f), new Vector2(0f, 1f)
+            new Vector2(0f, 0f), new Vector2(1f, 1f), new Vector2(1f, 1f)
         );
 
         estuaries.AddQuad(e2.v1, e1.v2, e2.v2, e1.v3);
         estuaries.AddQuad(e1.v3, e1.v4, e2.v4, e2.v5);
-        estuaries.AddQuadUV(0f, 0f, 0f, 1f);
-        estuaries.AddQuadUV(0f, 0f, 0f, 1f);
-
+        estuaries.AddQuadUV(
+            new Vector2(0f, 1f), new Vector2(0f, 0f),
+            new Vector2(1f, 1f), new Vector2(0f, 0f)
+        );
+        estuaries.AddQuadUV(
+            new Vector2(0f, 0f), new Vector2(0f, 0f),
+            new Vector2(1f, 1f), new Vector2(0f, 1f)
+        );
         estuaries.AddTriangleUV2(
             new Vector2(0.5f, 1.1f),
             new Vector2(1f, 0.8f),
