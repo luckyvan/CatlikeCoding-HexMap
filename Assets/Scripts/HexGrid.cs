0e973c0069c8a354547841ee11046bb438ebb50c
luckyvan <fanchou1215@gmail.com>
2018-01-12 14:48:45 +0800

    12.1.6 Creating New Maps

--- Assets/Scripts/HexGrid.cs
+++ Assets/Scripts/HexGrid.cs
@@ -213,4 +213,5 @@ public class HexGrid : MonoBehaviour
             chunks[i].Refresh();
         }
     }
+
 }
--- Assets/Scripts/NewMapMenu.cs
+++ Assets/Scripts/NewMapMenu.cs
@@ -15,4 +15,25 @@ public class NewMapMenu : MonoBehaviour {
     {
         gameObject.SetActive(false);
     }
+
+    void CreateMap(int x, int z)
+    {
+        hexGrid.CreateMap(x, z);
+        Close();
+    }
+
+    public void CreateSmallMap()
+    {
+        CreateMap(20, 15);
+    }
+
+    public void CreateMediumMap()
+    {
+        CreateMap(40, 30);
+    }
+
+    public void CreateLargeMap()
+    {
+        CreateMap(80, 60);
+    }
 }
