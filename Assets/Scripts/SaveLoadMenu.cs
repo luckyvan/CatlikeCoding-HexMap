e754a4b61ffdc6a65827fb086a1057e90133a1b2
luckyvan <fanchou1215@gmail.com>
2018-01-13 16:43:13 +0800

    13.3.4 Saving and Loading 13.3.5 Map Items

--- Assets/Scripts/SaveLoadMenu.cs
+++ Assets/Scripts/SaveLoadMenu.cs
@@ -45,4 +45,63 @@ public class SaveLoadMenu : MonoBehaviour
         }
         return Path.Combine(Application.persistentDataPath, mapName + ".map");
     }
+
+    void Save(string path)
+    {
+        //		string path = Path.Combine(Application.persistentDataPath, "test.map");
+        using (
+            BinaryWriter writer =
+            new BinaryWriter(File.Open(path, FileMode.Create))
+        )
+        {
+            writer.Write(1);
+            hexGrid.Save(writer);
+        }
+    }
+
+    void Load(string path)
+    {
+        if (!File.Exists(path))
+        {
+            Debug.LogError("File does not exist " + path);
+            return;
+        }
+        //		string path = Path.Combine(Application.persistentDataPath, "test.map");
+        using (BinaryReader reader = new BinaryReader(File.OpenRead(path)))
+        {
+            int header = reader.ReadInt32();
+            if (header <= 1)
+            {
+                hexGrid.Load(reader, header);
+                HexMapCamera.ValidatePosition();
+            }
+            else
+            {
+                Debug.LogWarning("Unknown map format " + header);
+            }
+        }
+    }
+
+    public void Action()
+    {
+        string path = GetSelectedPath();
+        if (path == null)
+        {
+            return;
+        }
+        if (saveMode)
+        {
+            Save(path);
+        }
+        else
+        {
+            Load(path);
+        }
+        Close();
+    }
+
+    public void SelectItem(string name)
+    {
+        nameInput.text = name;
+    }
 }
