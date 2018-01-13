971307099cb5324c173d769fb8be69ba6ea1d281
luckyvan <fanchou1215@gmail.com>
2018-01-13 16:01:02 +0800

    13.3.1 Controlling the Menu

---
+++ Assets/Scripts/SaveLoadMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class SaveLoadMenu : MonoBehaviour
+{
+
+    public HexGrid hexGrid;
+
+    bool saveMode;
+
+    public void Open(bool saveMode)
+    {
+        this.saveMode = saveMode;
+        gameObject.SetActive(true);
+        HexMapCamera.Locked = true;
+    }
+
+    public void Close()
+    {
+        gameObject.SetActive(false);
+        HexMapCamera.Locked = false;
+    }
+}
