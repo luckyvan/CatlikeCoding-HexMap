a24e62a8e730e0fffee87fcbcf897d9d43b5f6e3
luckyvan <fanchou1215@gmail.com>
2018-01-15 16:35:53 +0800

    14.2 Texture Arrays 14.2.1 Wizard

---
+++ Assets/Editor/TextureArrayWizzard.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class TextureArrayWizard : ScriptableWizard
+{
+    public Texture2D[] textures;
+
+    [MenuItem("Assets/Create/Texture Array")]
+    static void CreateWizard()
+    {
+        ScriptableWizard.DisplayWizard<TextureArrayWizard>(
+            "Create Texture Array", "Create"
+        );
+    }
+}
