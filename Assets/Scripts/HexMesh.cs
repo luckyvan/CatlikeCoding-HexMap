ee3b81b9b3ba9bf377a8a99353e35dcb6396b6c8
luckyvan <fanchou1215@gmail.com>
2018-01-16 09:24:51 +0800

    14.3 Selecting Textures 14.3.1 Mesh Data

--- Assets/Scripts/HexMesh.cs
+++ Assets/Scripts/HexMesh.cs
@@ -7,13 +7,14 @@ public class HexMesh : MonoBehaviour
 {
 
     Mesh hexMesh;
-    [NonSerialized] List<Vector3> vertices;
+    [NonSerialized] List<Vector3> vertices, terrainTypes;
     [NonSerialized] List<Color> colors;
     [NonSerialized] List<int> triangles;
     [NonSerialized] List<Vector2> uvs, uv2s;
 
     MeshCollider meshCollider;
     public bool useCollider, useColors, useUVCoordinates, useUV2Coordinates;
+    public bool useTerrainTypes;
 
     void Awake()
     {
@@ -164,6 +165,10 @@ public class HexMesh : MonoBehaviour
         {
             uv2s = ListPool<Vector2>.Get();
         }
+        if (useTerrainTypes)
+        {
+            terrainTypes = ListPool<Vector3>.Get();
+        }
         triangles = ListPool<int>.Get();
     }
 
@@ -194,6 +199,11 @@ public class HexMesh : MonoBehaviour
             hexMesh.SetUVs(0, uv2s);
             ListPool<Vector2>.Add(uv2s);
         }
+        if (useTerrainTypes)
+        {
+            hexMesh.SetUVs(2, terrainTypes);
+            ListPool<Vector3>.Add(terrainTypes);
+        }
     }
 
     public void AddQuadUnperturbed(
@@ -211,5 +221,20 @@ public class HexMesh : MonoBehaviour
         triangles.Add(vertexIndex + 1);
         triangles.Add(vertexIndex + 2);
         triangles.Add(vertexIndex + 3);
+    }
+
+    public void AddTriangleTerrainTypes(Vector3 types)
+    {
+        terrainTypes.Add(types);
+        terrainTypes.Add(types);
+        terrainTypes.Add(types);
+    }
+
+    public void AddQuadTerrainTypes(Vector3 types)
+    {
+        terrainTypes.Add(types);
+        terrainTypes.Add(types);
+        terrainTypes.Add(types);
+        terrainTypes.Add(types);
     }
 }
