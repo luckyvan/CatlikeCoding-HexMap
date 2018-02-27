176fe623487eaf58706a93b706ec3e3462daa14f
luckyvan <fanchou1215@gmail.com>
2018-02-27 22:06:04 +0800

    18.1.4 Unit Orientation

--- Assets/Scripts/HexMapEditor.cs
+++ Assets/Scripts/HexMapEditor.cs
@@ -344,6 +344,7 @@ public class HexMapEditor : MonoBehaviour
             HexUnit unit = Instantiate(unitPrefab);
             unit.transform.SetParent(hexGrid.transform, false);
             unit.Location = cell;
+            unit.Orientation = Random.Range(0f, 360f);
         }
 
     }
--- Assets/Scripts/HexUnit.cs
+++ Assets/Scripts/HexUnit.cs
@@ -13,4 +13,19 @@ public class HexUnit : MonoBehaviour
         }
     }
     HexCell location;
+
+    public float Orientation
+    {
+        get
+        {
+            return orientation;
+        }
+        set
+        {
+            orientation = value;
+            transform.localRotation = Quaternion.Euler(0f, value, 0f);
+        }
+    }
+
+    float orientation;
 }
