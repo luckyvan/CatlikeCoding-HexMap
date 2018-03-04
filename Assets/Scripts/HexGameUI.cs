d449c76484c9ff87e4506443728445ff4c14dad6
luckyvan <fanchou1215@gmail.com>
2018-03-04 21:42:37 +0800

    18.3.2 Game UI

---
+++ Assets/Scripts/HexGameUI.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HexGameUI : MonoBehaviour
+{
+
+    public HexGrid grid;
+
+    public void SetEditMode(bool toggle)
+    {
+        enabled = !toggle;
+        grid.ShowUI(!toggle);
+    }
+}
