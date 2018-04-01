66bc6c4490111195bef360f5cfd282e9542bd1d7
luckyvan <fanchou1215@gmail.com>
2018-04-01 18:01:15 +0800

    21.1.1 Toggling Visibility

--- Assets/Scripts/HexGameUI.cs
+++ Assets/Scripts/HexGameUI.cs
@@ -15,6 +15,15 @@ public class HexGameUI : MonoBehaviour
         enabled = !toggle;
         grid.ShowUI(!toggle);
         grid.ClearPath();
+
+        if (toggle)
+        {
+            Shader.EnableKeyword("HEX_MAP_EDIT_MODE");
+        }
+        else
+        {
+            Shader.DisableKeyword("HEX_MAP_EDIT_MODE");
+        }
     }
 
     bool UpdateCurrentCell()
