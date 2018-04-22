ecc1598ee0af5c7a731f1e5d3e7a81e2c0a8d36e
luckyvan <fanchou1215@gmail.com>
2018-04-22 08:24:12 +0800

    23.2.2 Creating a Single Chunk

--- Assets/Scripts/HexMapGenerator.cs
+++ Assets/Scripts/HexMapGenerator.cs
@@ -8,19 +8,54 @@ public class HexMapGenerator : MonoBehaviour {
 
     int cellCount;
 
+    HexCellPriorityQueue searchFrontier;
+
+    int searchFrontierPhase;
+
     public void GenerateMap(int x, int z)
     {
         cellCount = x * z;
         grid.CreateMap(x, z);
+        if (searchFrontier == null)
+        {
+            searchFrontier = new HexCellPriorityQueue();
+        }
         RaiseTerrain(7);
+        for (int i = 0; i < cellCount; i++)
+        {
+            grid.GetCell(i).SearchPhase = 0;
+        }
     }
 
     void RaiseTerrain(int chunkSize)
     {
-        for (int i = 0; i < chunkSize; i++)
+        searchFrontierPhase += 1;
+        HexCell firstCell = GetRandomCell();
+        firstCell.SearchPhase = searchFrontierPhase;
+        firstCell.Distance = 0;
+        firstCell.SearchHeuristic = 0;
+        searchFrontier.Enqueue(firstCell);
+
+        int size = 0;
+        while (size < chunkSize && searchFrontier.Count > 0)
         {
-            GetRandomCell().TerrainTypeIndex = 1;
+            HexCell current = searchFrontier.Dequeue();
+            current.TerrainTypeIndex = 1;
+            size += 1;
+
+            for (HexDirection d = HexDirection.NE; d <= HexDirection.NW; d++)
+            {
+                HexCell neighbor = current.GetNeighbor(d);
+                if (neighbor && neighbor.SearchPhase < searchFrontierPhase)
+                {
+                    neighbor.SearchPhase = searchFrontierPhase;
+                    neighbor.Distance = 0;
+                    neighbor.SearchHeuristic = 0;
+                    searchFrontier.Enqueue(neighbor);
+                }
+            }
         }
+        searchFrontier.Clear();
     }
 
     HexCell GetRandomCell()
